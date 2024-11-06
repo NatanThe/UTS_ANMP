313570e622bbb61dfe274c18dc0e6586b2f63d16
Natan <s160820013@student.ubaya.ac.id>
2024-11-07 00:54:13 +0700

    project build

---
+++ app/src/main/java/com/example/uts2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.uts2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.uts2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding.bottomNav.setupWithNavController(navController)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view) // change the argument of setContentView
+
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
+                || super.onSupportNavigateUp()
+    }
+
+}
---
+++ app/src/main/java/com/example/uts2/ScheduleFragment.kt
@@ -0,0 +1,28 @@
+package com.example.uts2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.uts2.databinding.FragmentGameBinding
+import com.example.uts2.databinding.FragmentScheduleBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ScheduleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ScheduleFragment : Fragment() {
+    private lateinit var binding: FragmentScheduleBinding
+    override fun onCreateView(inflater: LayoutInflater, container:
+    ViewGroup?,savedInstanceState: Bundle?): View? {
+        binding = FragmentScheduleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
